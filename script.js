b27a1dd7fc469939a278d3718cc8ebc651aadc43
jtsapos <jtsapos@gmx.com>
2023-01-11 11:35:56 -0500

    Updated script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 	// select main-nav
-    const mainNav = document.querySelector('.main-nav')
-    const visitSite = document.querySelector('.visit-site')
-    // const menu = document.getElementById('menu')
+    const mainNav = document.querySelector('.main-nav');
+    const visitSite = document.querySelector('.visit-site');
+    const back = document.getElementById('back');
     const toggle = document.getElementById('toggle');
 
     document.onclick = function (e) {
          if (e.target === toggle || e.target.className === 'bar') {
             toggle.classList.toggle('active');
             mainNav.classList.toggle('active');
-         } else if (e.target.className === 'visit-site') {
+         } else if (e.target.className === 'visit-site' || e.target === back) {
             visitSite.classList.toggle('active');
+            back.classList.toggle('active');
          } else if (e.target !== mainNav ) {
             toggle.classList.toggle('active');
             mainNav.classList.toggle('active');
